0de0e340e39849364829f5aa743ddab2bc7862a2
Carina Fernandes <carina.fernandes@outlook.com>
2025-01-24 18:27:25 -0300

    subscribe

--- src/app/shared/food-list/food-list.component.ts
+++ src/app/shared/food-list/food-list.component.ts
@@ -13,6 +13,11 @@ export class FoodListComponent implements OnInit {
 
   ngOnInit(): void {
     this.foodList = this.foodListService.foodList();
+
+    this.foodListService.emitEvent.subscribe(
+      res => alert(`Você adicionou ${res}`)
+      
+    );
   }
 
 }
